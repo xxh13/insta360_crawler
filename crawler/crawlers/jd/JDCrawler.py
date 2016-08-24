d1a029f4adda1bb0480c2c18f2e47f2f60c459e9
CielNi <562421167@qq.com>
2016-08-24 16:36:43 +0800

    update JDCrawler.py

--- crawler/crawlers/jd/JDCrawler.py
+++ crawler/crawlers/jd/JDCrawler.py
@@ -29,7 +29,7 @@ class JDCrawler:
             self.cap["phantomjs.page.settings.localToRemoteUrlAccessEnabled"] = True
             self.cap["userAgent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"
             self.cap["XSSAuditingEnabled"] = True
-            self.driver = webdriver.PhantomJS(desired_capabilities=self.cap)
+            self.driver = webdriver.PhantomJS(desired_capabilities=self.cap, service_args=['--ignore-ssl-errors=true','--ssl-protocol=any', '--web-security=true'])
             # self.driver = webdriver.Chrome()
         def main(self):
             products = ['insta360 Nano', 'Gear 360', 'theta', 'LG 360 CAM']
