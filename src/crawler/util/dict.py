cade166b3fd6cfc51070fdb9b2f1368ae9fafc86
NiShuang <klqbtnsns@hotmail.com>
2017-06-17 17:52:19 +0800

    power manage

--- src/crawler/util/dict.py
+++ src/crawler/util/dict.py
@@ -12,8 +12,7 @@ media_dict = {
 }
 
 group_dict = {
-    'abroad_sales': '录入系统->国外销售录入',
-    'electronic_sales': '录入系统->电商销售录入',
+
     'nano_activation': 'BI->设备激活情况',
     'nano_history': 'BI->历史总数',
     'nano_link': 'BI->购买链接',
@@ -23,5 +22,8 @@ group_dict = {
     'nano_share': 'BI->内容分享情况',
     'nano_support': 'BI->服务与支持',
     'nano_use_condition': 'BI->App使用情况',
-    'native_sales': '录入系统->国内销售录入'
+    'divided_share' '内容分享后台'
+    'native_sales': '录入系统->国内销售录入',
+    'abroad_sales': '录入系统->国外销售录入',
+    'electronic_sales': '录入系统->电商销售录入'
 }
